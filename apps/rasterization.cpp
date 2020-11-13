f1d5cf1a00a1b069795bbe0ee86e393b731f9c0c
yangfeng <yangfengzzz@hotmail.com>
2020-11-13 13:44:23 +0800

    add shadow

--- apps/rasterization.cpp
+++ apps/rasterization.cpp
@@ -1,76 +1,124 @@
 #include <vector>
+#include <limits>
 #include <iostream>
-
 #include "../src.raster/tgaimage.h"
 #include "../src.raster/model.h"
 #include "../src.raster/geometry.h"
 #include "../src.raster/our_gl.h"
 
-Model *model     = NULL;
+Model *model        = NULL;
+float *shadowbuffer = NULL;
+
 const int width  = 800;
 const int height = 800;
 
-Vec3f light_dir(1,1,1);
-Vec3f       eye(0,-1,3);
+Vec3f light_dir(1,1,0);
+Vec3f       eye(1,1,4);
 Vec3f    center(0,0,0);
 Vec3f        up(0,1,0);
 
 struct Shader : public IShader {
-    mat<2,3,float> varying_uv;  // same as above
     mat<4,4,float> uniform_M;   //  Projection*ModelView
     mat<4,4,float> uniform_MIT; // (Projection*ModelView).invert_transpose()
-
+    mat<4,4,float> uniform_Mshadow; // transform framebuffer screen coordinates to shadowbuffer screen coordinates
+    mat<2,3,float> varying_uv;  // triangle uv coordinates, written by the vertex shader, read by the fragment shader
+    mat<3,3,float> varying_tri; // triangle coordinates before Viewport transform, written by VS, read by FS
+    
+    Shader(Matrix M, Matrix MIT, Matrix MS) : uniform_M(M), uniform_MIT(MIT), uniform_Mshadow(MS), varying_uv(), varying_tri() {}
+    
     virtual Vec4f vertex(int iface, int nthvert) {
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
-        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
-        return Viewport*Projection*ModelView*gl_Vertex; // transform it to screen coordinates
+        Vec4f gl_Vertex = Viewport*Projection*ModelView*embed<4>(model->vert(iface, nthvert));
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex/gl_Vertex[3]));
+        return gl_Vertex;
     }
-
+    
     virtual bool fragment(Vec3f bar, TGAColor &color) {
-        Vec2f uv = varying_uv*bar;
-        Vec3f n = proj<3>(uniform_MIT*embed<4>(model->normal(uv))).normalize();
-        Vec3f l = proj<3>(uniform_M  *embed<4>(light_dir        )).normalize();
+        Vec4f sb_p = uniform_Mshadow*embed<4>(varying_tri*bar); // corresponding point in the shadow buffer
+        sb_p = sb_p/sb_p[3];
+        int idx = int(sb_p[0]) + int(sb_p[1])*width; // index in the shadowbuffer array
+        float shadow = .3+.7*(shadowbuffer[idx]<sb_p[2]+43.34); // magic coeff to avoid z-fighting
+        Vec2f uv = varying_uv*bar;                 // interpolate uv for the current pixel
+        Vec3f n = proj<3>(uniform_MIT*embed<4>(model->normal(uv))).normalize(); // normal
+        Vec3f l = proj<3>(uniform_M  *embed<4>(light_dir        )).normalize(); // light vector
         Vec3f r = (n*(n*l*2.f) - l).normalize();   // reflected light
         float spec = pow(std::max(r.z, 0.0f), model->specular(uv));
         float diff = std::max(0.f, n*l);
         TGAColor c = model->diffuse(uv);
-        color = c;
-        for (int i=0; i<3; i++) color[i] = std::min<float>(5 + c[i]*(diff + .6*spec), 255);
+        for (int i=0; i<3; i++) color[i] = std::min<float>(20 + c[i]*shadow*(1.2*diff + .6*spec), 255);
         return false;
     }
 };
 
-int main(int argc, char** argv) {
-    if (2==argc) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("../obj/african_head/african_head.obj");
+struct DepthShader : public IShader {
+    mat<3,3,float> varying_tri;
+    
+    DepthShader() : varying_tri() {}
+    
+    virtual Vec4f vertex(int iface, int nthvert) {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+        gl_Vertex = Viewport*Projection*ModelView*gl_Vertex;          // transform it to screen coordinates
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex/gl_Vertex[3]));
+        return gl_Vertex;
     }
     
-    lookat(eye, center, up);
-    viewport(width/8, height/8, width*3/4, height*3/4);
-    projection(-1.f/(eye-center).norm());
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        Vec3f p = varying_tri*bar;
+        color = TGAColor(255, 255, 255)*(p.z/depth);
+        return false;
+    }
+};
+
+int main(int argc, char** argv) {    
+    float *zbuffer = new float[width*height];
+    shadowbuffer   = new float[width*height];
+    for (int i=width*height; --i; ) {
+        zbuffer[i] = shadowbuffer[i] = -std::numeric_limits<float>::max();
+    }
+    
+    model = new Model("../obj/african_head/african_head.obj");
     light_dir.normalize();
     
-    TGAImage image  (width, height, TGAImage::RGB);
-    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
-    
-    Shader shader;
-    shader.uniform_M   =  Projection*ModelView;
-    shader.uniform_MIT = (Projection*ModelView).invert_transpose();
-    for (int i=0; i<model->nfaces(); i++) {
+    { // rendering the shadow buffer
+        TGAImage depth(width, height, TGAImage::RGB);
+        lookat(light_dir, center, up);
+        viewport(width/8, height/8, width*3/4, height*3/4);
+        projection(0);
+        
+        DepthShader depthshader;
         Vec4f screen_coords[3];
-        for (int j=0; j<3; j++) {
-            screen_coords[j] = shader.vertex(i, j);
+        for (int i=0; i<model->nfaces(); i++) {
+            for (int j=0; j<3; j++) {
+                screen_coords[j] = depthshader.vertex(i, j);
+            }
+            triangle(screen_coords, depthshader, depth, shadowbuffer);
         }
-        triangle(screen_coords, shader, image, zbuffer);
+        depth.flip_vertically(); // to place the origin in the bottom left corner of the image
+        depth.write_tga_file("depth.tga");
     }
     
-    image.  flip_vertically(); // to place the origin in the bottom left corner of the image
-    zbuffer.flip_vertically();
-    image.  write_tga_file("output.tga");
-    zbuffer.write_tga_file("zbuffer.tga");
+    Matrix M = Viewport*Projection*ModelView;
+    
+    { // rendering the frame buffer
+        TGAImage frame(width, height, TGAImage::RGB);
+        lookat(eye, center, up);
+        viewport(width/8, height/8, width*3/4, height*3/4);
+        projection(-1.f/(eye-center).norm());
+        
+        Shader shader(ModelView, (Projection*ModelView).invert_transpose(), M*(Viewport*Projection*ModelView).invert());
+        Vec4f screen_coords[3];
+        for (int i=0; i<model->nfaces(); i++) {
+            for (int j=0; j<3; j++) {
+                screen_coords[j] = shader.vertex(i, j);
+            }
+            triangle(screen_coords, shader, frame, zbuffer);
+        }
+        frame.flip_vertically(); // to place the origin in the bottom left corner of the image
+        frame.write_tga_file("framebuffer.tga");
+    }
     
     delete model;
+    delete [] zbuffer;
+    delete [] shadowbuffer;
     return 0;
 }
--- src.raster/geometry.h
+++ src.raster/geometry.h
@@ -9,7 +9,7 @@ template<size_t DimCols,size_t DimRows,typename T> class mat;
 
 template <size_t DIM, typename T> struct vec {
     vec() { for (size_t i=DIM; i--; data_[i] = T()); }
-          T& operator[](const size_t i)       { assert(i<DIM); return data_[i]; }
+    T& operator[](const size_t i)       { assert(i<DIM); return data_[i]; }
     const T& operator[](const size_t i) const { assert(i<DIM); return data_[i]; }
 private:
     T data_[DIM];
@@ -21,9 +21,9 @@ template <typename T> struct vec<2,T> {
     vec() : x(T()), y(T()) {}
     vec(T X, T Y) : x(X), y(Y) {}
     template <class U> vec<2,T>(const vec<2,U> &v);
-          T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
+    T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
     const T& operator[](const size_t i) const { assert(i<2); return i<=0 ? x : y; }
-
+    
     T x,y;
 };
 
@@ -33,11 +33,11 @@ template <typename T> struct vec<3,T> {
     vec() : x(T()), y(T()), z(T()) {}
     vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
     template <class U> vec<3,T>(const vec<3,U> &v);
-          T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+    T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
     const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
     float norm() { return std::sqrt(x*x+y*y+z*z); }
     vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
-
+    
     T x,y,z;
 };
 
@@ -115,62 +115,72 @@ template<size_t DimRows,size_t DimCols,typename T> class mat {
     vec<DimCols,T> rows[DimRows];
 public:
     mat() {}
-
+    
     vec<DimCols,T>& operator[] (const size_t idx) {
         assert(idx<DimRows);
         return rows[idx];
     }
-
+    
     const vec<DimCols,T>& operator[] (const size_t idx) const {
         assert(idx<DimRows);
         return rows[idx];
     }
-
+    
     vec<DimRows,T> col(const size_t idx) const {
         assert(idx<DimCols);
         vec<DimRows,T> ret;
         for (size_t i=DimRows; i--; ret[i]=rows[i][idx]);
         return ret;
     }
-
+    
     void set_col(size_t idx, vec<DimRows,T> v) {
         assert(idx<DimCols);
         for (size_t i=DimRows; i--; rows[i][idx]=v[i]);
     }
-
+    
     static mat<DimRows,DimCols,T> identity() {
         mat<DimRows,DimCols,T> ret;
         for (size_t i=DimRows; i--; )
-            for (size_t j=DimCols;j--; ret[i][j]=(i==j));
+        for (size_t j=DimCols;j--; ret[i][j]=(i==j));
         return ret;
     }
-
+    
     T det() const {
         return dt<DimCols,T>::det(*this);
     }
-
+    
     mat<DimRows-1,DimCols-1,T> get_minor(size_t row, size_t col) const {
         mat<DimRows-1,DimCols-1,T> ret;
         for (size_t i=DimRows-1; i--; )
-            for (size_t j=DimCols-1;j--; ret[i][j]=rows[i<row?i:i+1][j<col?j:j+1]);
+        for (size_t j=DimCols-1;j--; ret[i][j]=rows[i<row?i:i+1][j<col?j:j+1]);
         return ret;
     }
-
+    
     T cofactor(size_t row, size_t col) const {
         return get_minor(row,col).det()*((row+col)%2 ? -1 : 1);
     }
-
+    
     mat<DimRows,DimCols,T> adjugate() const {
         mat<DimRows,DimCols,T> ret;
         for (size_t i=DimRows; i--; )
-            for (size_t j=DimCols; j--; ret[i][j]=cofactor(i,j));
+        for (size_t j=DimCols; j--; ret[i][j]=cofactor(i,j));
         return ret;
     }
-
+    
     mat<DimRows,DimCols,T> invert_transpose() {
         mat<DimRows,DimCols,T> ret = adjugate();
         T tmp = ret[0]*rows[0];
         return ret/tmp;
+    }
+    
+    mat<DimCols,DimRows,T> invert() {
+        return invert_transpose().transpose();
+    }
+    
+    mat<DimCols,DimRows,T> transpose() {
+        mat<DimCols,DimRows,T> ret;
+        for (size_t i=DimRows; i--; ret[i]=this->col(i));
+        return ret;
     }
 };
 
@@ -185,7 +195,7 @@ template<size_t DimRows,size_t DimCols,typename T> vec<DimRows,T> operator*(cons
 template<size_t R1,size_t C1,size_t C2,typename T>mat<R1,C2,T> operator*(const mat<R1,C1,T>& lhs, const mat<C1,C2,T>& rhs) {
     mat<R1,C2,T> result;
     for (size_t i=R1; i--; )
-        for (size_t j=C2; j--; result[i][j]=lhs[i]*rhs.col(j));
+    for (size_t j=C2; j--; result[i][j]=lhs[i]*rhs.col(j));
     return result;
 }
 
